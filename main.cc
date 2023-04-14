0f21626e3fa96a2bd31564c3915c87954cd253cf
Jayden Wong <jaywong8288@gmail.com>
2023-04-13 19:22:25 -0700

    rendering a simple red sphere

--- main.cc
+++ main.cc
@@ -4,9 +4,22 @@
 
 #include <iostream>
 
+bool hit_sphere(const point3& center, double radius, const ray& r){
+    vec3 oc = r.origin() - center;
+    auto a = dot(r.direction(),r.direction());
+    auto b = 2.0 * dot(oc, r.direction());
+    auto c = dot(oc,oc) - radius*radius;
+    auto discriminant = b*b - 4*a*c;
+    return (discriminant > 0);
+}
+
 color ray_color(const ray& r){
+    if(hit_sphere(point3(0,0,-1), 0.5, r))
+        return color(1,0,0);
     vec3 unit_direction = unit_vector(r.direction());
     auto t = 0.5*(unit_direction.y() + 1.0);
+    //linear interpolation
+    // blendedValue = (1 - t)*startValue + t*endValue
     return (1.0-t)*color(1.0,1.0,1.0) + t*color(0.5,0.7,1.0);
 }
 
@@ -15,7 +28,7 @@ using namespace std;
 int main(){
     // Image
     const auto aspect_ratio = 16.0/9.0;
-    const int image_width = 400;
+    const int image_width = 1280;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
 
     // Camera
