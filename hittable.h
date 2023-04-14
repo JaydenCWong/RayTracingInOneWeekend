ca37ea5a26483217bdf4ac252607a8362ed8559f
Jayden Wong <jaywong8288@gmail.com>
2023-04-13 19:48:38 -0700

    front faces vs. back faces

--- hittable.h
+++ hittable.h
@@ -7,6 +7,35 @@ struct hit_record{
     point3 p;
     vec3 normal;
     double t;
+    bool front_face;
+
+    inline void set_face_normal(const ray& r, const vec3& outward_normal){
+        //the intuition for comparing the ray to the normal
+        /*
+        if (dot(ray_direction, outward_normal) > 0.0) {
+            // ray is inside the sphere
+            ...
+        } else {
+            // ray is outside the sphere
+            ...
+        }
+        */
+       // well how do we remember the normal?
+       /*
+       bool front_face;
+        if (dot(ray_direction, outward_normal) > 0.0) {
+            // ray is inside the sphere
+            normal = -outward_normal;
+            front_face = false;
+        } else {
+            // ray is outside the sphere
+            normal = outward_normal;
+            front_face = true;
+        }*/
+        
+        front_face = dot(r.direction(), outward_normal) < 0;
+        normal = front_face ? outward_normal : -outward_normal;
+    }
 };
 
 class hittable{
