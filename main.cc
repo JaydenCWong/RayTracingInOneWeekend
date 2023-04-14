11e382b350af89b98f5002c37eb5c40298f92920
Jayden Wong <jaywong8288@gmail.com>
2023-04-13 20:32:35 -0700

    antialiasing - sweet lord it gets slow

--- main.cc
+++ main.cc
@@ -1,5 +1,6 @@
 #include "rtweekend.h"
 
+#include "camera.h"
 #include "color.h"
 #include "hittable_list.h"
 #include "sphere.h"
@@ -25,8 +26,9 @@ using namespace std;
 int main(){
     // Image
     const auto aspect_ratio = 16.0/9.0;
-    const int image_width = 1920;
+    const int image_width = 400;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
+    const int samples_per_pixel = 100;
 
     // World
     hittable_list world;
@@ -35,27 +37,24 @@ int main(){
 
     // Camera
 
-    auto viewport_height = 2.0;
-    auto viewport_width = aspect_ratio * viewport_height;
-    auto focal_length = 1.0;
-
-    auto origin = point3(0,0,0);
-    auto horizontal = vec3(viewport_width,0,0);
-    auto vertical = vec3(0,viewport_height,0);
-    auto lower_left_corner  = origin - horizontal/2 - vertical/2 - vec3(0,0,focal_length);
+    camera cam;
 
     // Render
 
-    cout << "P3\n" << image_width << ' ' << image_height << "\n 256\n";
+    cout << "P3\n" << image_width << ' ' << image_height << "\n 255\n";
 
     for(int j = image_height-1; j >= 0; --j){
         cerr << "\rScanlines remaining: " << j << ' ' << flush;
         for(int i = 0 ; i < image_width; ++i){
-            auto u = double(i) / (image_width - 1);
-            auto v = double(j) / (image_height-1);
-            ray r(origin, lower_left_corner + u*horizontal + v*vertical - origin);
-            color pixel_color = ray_color(r, world);
-            write_color(cout, pixel_color);
+            color pixel_color(0,0,0);
+            for(int s = 0; s < samples_per_pixel; s++){
+                auto u = (i + random_double()) / (image_width - 1);
+                auto v = (j + random_double()) / (image_height-1);
+                ray r = cam.get_ray(u,v);
+                pixel_color += ray_color(r, world);
+            }
+
+            write_color(cout, pixel_color, samples_per_pixel);
         }
     }
     cerr << "\nDone.\n";
