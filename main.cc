b07f2bb903c0fc8c1f9772f11237f788dce944dc
Jayden Wong <jaywong8288@gmail.com>
2023-05-03 22:42:25 -0600

    adding background

--- main.cc
+++ main.cc
@@ -9,7 +9,7 @@
 
 #include <iostream>
 
-color ray_color(const ray& r, const hittable& world, int depth){
+color ray_color(const ray& r, const color& background, const hittable& world, int depth){
     hit_record rec;
 
     // If we've exceeded the ray bounce limit, no more light is gathered
@@ -17,20 +17,18 @@ color ray_color(const ray& r, const hittable& world, int depth){
         return color(0,0,0);
     }
 
-    if(world.hit(r, 0.001, infinity, rec)){
-        ray scattered;
-        color attenuation;
-        if(rec.mat_ptr->scatter(r,rec, attenuation, scattered))
-            return attenuation * ray_color(scattered, world, depth-1);
-        return color(0,0,0);
-    }
+    // If the ray hits nothing, return the background color.
+    if(!world.hit(r,0.001, infinity, rec))
+        return background;
+    
+    ray scattered;
+    color attenuation;
+    color emitted = rec.mat_ptr->emitted(rec.u, rec.v, rec.p);
 
-    //background --- did not hit anything
-    vec3 unit_direction = unit_vector(r.direction());
-    auto t = 0.5*(unit_direction.y() + 1.0);
-    //linear interpolation
-    // blendedValue = (1 - t)*startValue + t*endValue
-    return (1.0-t)*color(1.0,1.0,1.0) + t*color(0.5,0.7,1.0);
+    if(!rec.mat_ptr->scatter(r,rec,attenuation,scattered))
+        return emitted;
+
+    return emitted + attenuation * ray_color(scattered,background,world,depth-1);
 }
 
 using namespace std;
@@ -127,10 +125,12 @@ int main(){
     point3 lookat;
     auto vfov = 40.0;
     auto aperture = 0.0;
+    color background(0,0,0);
 
     switch(0){
         case 1:
             world = random_scene();
+            background = color(0.7, 0.8, 1.00);
             lookfrom = point3(13,2,3);
             lookat = point3(0,0,0);
             vfov = 20.0;
@@ -139,6 +139,7 @@ int main(){
         
         case 2:
             world = two_spheres();
+            background = color(0.7, 0.8, 1.00);
             lookfrom = point3(13,2,3);
             lookat = point3(0,0,0);
             vfov = 20.0;
@@ -146,16 +147,22 @@ int main(){
 
         case 3:
             world = two_perlin_spheres();
+            background = color(0.7, 0.8, 1.00);
+            lookfrom = point3(13,2,3);
+            lookat = point3(0,0,0);
+            vfov = 20.0;
+            break;
+
+        case 4:
+            world = earth();
+            background = color(0.7, 0.8, 1.00);
             lookfrom = point3(13,2,3);
             lookat = point3(0,0,0);
             vfov = 20.0;
             break;
         default:
-        case 4:
-            world = earth();
-            lookfrom = point3(13,2,3);
-            lookat = point3(0,0,0);
-            vfov = 20.0;
+        case 5:
+            background = color(0.0, 0.0, 0.0);
             break;
     }
 
@@ -168,7 +175,7 @@ int main(){
 
     // Render
 
-    cout << "P3\n" << image_width << ' ' << image_height << "\n 255\n";
+    std::cout << "P3\n" << image_width << ' ' << image_height << "\n 255\n";
 
     for(int j = image_height-1; j >= 0; --j){
         cerr << "\rScanlines remaining: " << j << ' ' << flush;
@@ -178,10 +185,10 @@ int main(){
                 auto u = (i + random_double()) / (image_width - 1);
                 auto v = (j + random_double()) / (image_height-1);
                 ray r = cam.get_ray(u,v);
-                pixel_color += ray_color(r, world, max_depth);
+                pixel_color += ray_color(r, background, world, max_depth);
             }
 
-            write_color(cout, pixel_color, samples_per_pixel);
+            write_color(std::cout, pixel_color, samples_per_pixel);
         }
     }
     cerr << "\nDone.\n";
